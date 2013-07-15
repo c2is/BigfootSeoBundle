0a0ca019a90942d09c56bc4c133f6da2680cca03
sebastien.huot <sebastien.huot@c2is.fr>
2013-07-15 11:35:35 +0200

    Delete some comments (#master)

--- BigfootSeoBundle.php
+++ BigfootSeoBundle.php
@@ -10,12 +10,4 @@ use Bigfoot\Bundle\CoreBundle\Theme\Menu\Item;
  */
 class BigfootSeoBundle extends Bundle
 {
-    /**
-     * Add the entries to the back-office menu
-     */
-    public function boot()
-    {
-//        $this->container->get('theme')['sidebar']['menu']->addItem(new Item('metadata', 'Seo','admin_seo_metadata'));
-//        $this->container->get('theme')['sidebar']['menu']->addOnItem('sidebar_settings',new Item('sidebar_settings_metadata_parameter', 'Seo Parameters','admin_parameter_metadataparameter'));
-    }
 }

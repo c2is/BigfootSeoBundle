ef71c7c7141f6a6113852c37eea4e1f3aee7531d
Acrack <yehya.latti@gmail.com>
2014-01-27 08:58:18 +0100

    refacto

--- Listener/MenuListener.php
+++ Listener/MenuListener.php
@@ -2,25 +2,68 @@
 
 namespace Bigfoot\Bundle\SeoBundle\Listener;
 
-use Bigfoot\Bundle\CoreBundle\Event\MenuEvent;
-use Bigfoot\Bundle\CoreBundle\Theme\Menu\Item;
+use Symfony\Component\EventDispatcher\GenericEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-class MenuListener
+use Bigfoot\Bundle\CoreBundle\Event\MenuEvent;
+
+/**
+ * Menu Listener
+ */
+class MenuListener implements EventSubscriberInterface
 {
     /**
-     * Add entry to the sidebar menu
+     * Get subscribed events
      *
-     * @param MenuEvent $event
+     * @return array
      */
-    function onMenuGenerate(MenuEvent $event)
+    public static function getSubscribedEvents()
     {
-        $menu = $event->getMenu();
+        return array(
+            MenuEvent::GENERATE_MAIN => 'onGenerateMain',
+        );
+    }
 
-        if ($menu->getName() == 'sidebar_menu') {
+    /**
+     * @param GenericEvent $event
+     */
+    public function onGenerateMain(GenericEvent $event)
+    {
+        $menu    = $event->getSubject();
+        $seoMenu = $menu->getChild('seo');
 
-            $menu->addItem(new Item('metadata', 'Seo','admin_seo_metadata', array(), array(), 'rocket'));
-            $menu->addOnItem('sidebar_settings',new Item('sidebar_settings_metadata_parameter', 'Seo Parameters', 'admin_parameter_metadataparameter', array(), array(), 'rocket'));
-        }
+        $seoMenu->addChild(
+            'metadata',
+            array(
+                'label'  => 'Metadata',
+                'route'  => 'admin_metadata',
+                'extras' => array(
+                    'routes' => array(
+                        'admin_metadata_new',
+                        'admin_metadata_edit'
+                    )
+                ),
+                'linkAttributes' => array(
+                    'icon' => 'rocket',
+                )
+            )
+        );
 
+        $seoMenu->addChild(
+            'metadata_parameter',
+            array(
+                'label'  => 'Metadata Parameter',
+                'route'  => 'admin_parameter_metadataparameter',
+                'extras' => array(
+                    'routes' => array(
+                        'admin_parameter_metadataparameter_new',
+                        'admin_parameter_metadataparameter_edit'
+                    )
+                ),
+                'linkAttributes' => array(
+                    'icon' => 'rocket',
+                )
+            )
+        );
     }
 }

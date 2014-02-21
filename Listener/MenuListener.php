c23bf60478302f4d4f124076d3fb56028795b10f
Guillaume Manen <g.manen@c2is.fr>
2014-02-21 12:08:54 +0100

    Menu access restricted to admin

--- Listener/MenuListener.php
+++ Listener/MenuListener.php
@@ -6,12 +6,26 @@ use Symfony\Component\EventDispatcher\GenericEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 use Bigfoot\Bundle\CoreBundle\Event\MenuEvent;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
 /**
  * Menu Listener
  */
 class MenuListener implements EventSubscriberInterface
 {
+    /**
+     * @var \Symfony\Component\Security\Core\SecurityContextInterface
+     */
+    private $security;
+
+    /**
+     * @param SecurityContextInterface $security
+     */
+    public function __construct(SecurityContextInterface $security)
+    {
+        $this->security = $security;
+    }
+
     /**
      * Get subscribed events
      *
@@ -32,38 +46,40 @@ class MenuListener implements EventSubscriberInterface
         $menu    = $event->getSubject();
         $seoMenu = $menu->getChild('seo');
 
-        $seoMenu->addChild(
-            'metadata',
-            array(
-                'label'  => 'Metadata',
-                'route'  => 'admin_seo_metadata',
-                'extras' => array(
-                    'routes' => array(
-                        'admin_seo_metadata_new',
-                        'admin_seo_metadata_edit'
+        if ($this->security->isGranted('ROLE_ADMIN')) {
+            $seoMenu->addChild(
+                'metadata',
+                array(
+                    'label'  => 'Metadata',
+                    'route'  => 'admin_seo_metadata',
+                    'extras' => array(
+                        'routes' => array(
+                            'admin_seo_metadata_new',
+                            'admin_seo_metadata_edit'
+                        )
+                    ),
+                    'linkAttributes' => array(
+                        'icon' => 'rocket',
                     )
-                ),
-                'linkAttributes' => array(
-                    'icon' => 'rocket',
                 )
-            )
-        );
+            );
 
-        $seoMenu->addChild(
-            'metadata_parameter',
-            array(
-                'label'  => 'Metadata Parameter',
-                'route'  => 'admin_parameter_metadataparameter',
-                'extras' => array(
-                    'routes' => array(
-                        'admin_parameter_metadataparameter_new',
-                        'admin_parameter_metadataparameter_edit'
+            $seoMenu->addChild(
+                'metadata_parameter',
+                array(
+                    'label'  => 'Metadata Parameter',
+                    'route'  => 'admin_parameter_metadataparameter',
+                    'extras' => array(
+                        'routes' => array(
+                            'admin_parameter_metadataparameter_new',
+                            'admin_parameter_metadataparameter_edit'
+                        )
+                    ),
+                    'linkAttributes' => array(
+                        'icon' => 'rocket',
                     )
-                ),
-                'linkAttributes' => array(
-                    'icon' => 'rocket',
                 )
-            )
-        );
+            );
+        }
     }
 }

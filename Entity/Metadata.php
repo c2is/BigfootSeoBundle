254e38321682d7061428b5a5b4a186bb0d1ecb09
Guillaume Manen <g.manen@c2is.fr>
2014-12-12 11:35:51 +0100

    Refactoring : PostgreSQL

--- Entity/Metadata.php
+++ Entity/Metadata.php
@@ -18,7 +18,7 @@ class Metadata
      *
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\GeneratedValue(strategy="IDENTITY")
      */
     private $id;
 
